cd0d5de43f2c904772356087bc108888c88586bc
Dustin Koch <dustin.koch@me.com>
2019-06-01 10:25:54 -0500

    game logic complete

--- SwitchColors/Scenes/GameScene.swift
+++ SwitchColors/Scenes/GameScene.swift
@@ -8,11 +8,27 @@
 
 import SpriteKit
 
+enum PlayColors {
+    static let colors = [
+    UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0),
+    UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),
+    UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0),
+    UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+    ]
+}
+
+enum SwitchState: Int {
+    case red, yellow, green, blue
+}
+
 class GameScene: SKScene {
     
     //MARK: - Properties
     var colorSwitch = SKSpriteNode()
-    
+    var switchState = SwitchState.red
+    var currentColorIndex: Int?
+    let scoreLabel = SKLabelNode(text: "0")
+    var score = 0
 
     override func didMove(to view: SKView) {
         layoutScene()
@@ -22,6 +38,7 @@ class GameScene: SKScene {
     
     func setupPhysics() {
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.5)
+        physicsWorld.contactDelegate = self
     }
     
     func layoutScene() {
@@ -32,23 +49,74 @@ class GameScene: SKScene {
         colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: colorSwitch.size.width / 2)
         colorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
         colorSwitch.physicsBody?.isDynamic = false
-        
         addChild(colorSwitch)
+        
+        scoreLabel.fontName = "AvenirNext-Bold"
+        scoreLabel.fontSize = 60.0
+        scoreLabel.fontColor = UIColor.white
+        scoreLabel.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(scoreLabel)
+        
+        spawnBall()
+    }
+    
+    func updateScoreLabel() {
+        scoreLabel.text = "\(score)"
     }
 
     func spawnBall() {
-        let ball = SKSpriteNode(imageNamed: "ball")
+        currentColorIndex = Int(arc4random_uniform(UInt32(4)))
+        
+        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), color: PlayColors.colors[currentColorIndex!], size: CGSize(width: 30.0, height: 30.0))
+        ball.colorBlendFactor = 1.0
+        ball.name = "Ball"
         ball.position = CGPoint(x: frame.midX, y: frame.maxY)
-        ball.size = CGSize(width: 30.0, height: 30.0)
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
         ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
         ball.physicsBody?.contactTestBitMask = PhysicsCategories.switchCategory
         ball.physicsBody?.collisionBitMask = PhysicsCategories.none
         
-        
         addChild(ball)
     }
     
+    func turnWheel() {
+        if let newState = SwitchState(rawValue: switchState.rawValue + 1) {
+            switchState = newState
+        } else {
+            switchState = .red
+        }
+        colorSwitch.run(SKAction.rotate(byAngle: .pi / 2, duration: 0.25))
+        
+        
+        
+    }
     
+    func gameOver() {
+        print("GameOver Dude")
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        turnWheel()
+    }
+  
 }//END OF CLASS
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        if contactMask == PhysicsCategories.ballCategory | PhysicsCategories.switchCategory {
+            if let ball = contact.bodyA.node?.name == "Ball" ? contact.bodyA.node as? SKSpriteNode : contact.bodyB as? SKSpriteNode {
+                if currentColorIndex == switchState.rawValue {
+                    score += 1
+                    updateScoreLabel()
+                    ball.run(SKAction.fadeOut(withDuration: 0.25), completion:  {
+                        ball.removeFromParent()
+                        self.spawnBall()
+                    })
+                } else {
+                    gameOver()
+                }
+            }
+        }
+    }
+}
